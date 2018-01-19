465306a02e5b2b9b329975df99e13745d5edcab7
zmisgod <starzmisgod@gmail.com>
2018-01-19 16:15:13 +0800

    New

--- App/Controller/WechatController.php
+++ App/Controller/WechatController.php
@@ -14,17 +14,22 @@ class WechatController extends WechatAbstract
 
     public function server()
     {
-        $config = Config::getInstance()->getConfig('config.wechat');
-        $app = Factory::officialAccount($config);
-        $server = $app->server;
-        $user = $app->user;
+        if($_GET['echostr']){
+            $this->response()->write($_GET['echostr']);
+        }else {
+            $config = Config::getInstance()->getConfig('config.wechat');
+            $app    = Factory::officialAccount($config);
+            $server = $app->server;
+            $user   = $app->user;
 
-        $server->push(function($message) use ($user) {
-            $fromUser = $user->get($message['FromUserName']);
+            $server->push(function($message) use ($user) {
+                $fromUser = $user->get($message['FromUserName']);
 
-            return "{$fromUser->nickname} 您好！欢迎关注 zmisgod!";
-        });
+                return "{$fromUser->nickname} 您好！欢迎关注 zmisgod!";
+            });
 
-        $server->serve()->send();
+            $response = $server->serve();
+            $this->response()->write($response->getContent());
+        }
     }
 }
